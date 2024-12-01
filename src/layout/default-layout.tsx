1842b0bafef4f8b47d73b23c6c3dd1572e668842
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-12-01 22:19:09 +0700

    feature(header layout): handle background when scroll page

--- src/layout/default-layout.tsx
+++ src/layout/default-layout.tsx
@@ -1,18 +1,43 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from '~components';
+import { cn } from '~utils';
 import Footer from './footer';
 import Header from './header';
 
 function DefaultLayout() {
+    const [isHasBg, setIsHasBg] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 80) {
+                setIsHasBg(true);
+            } else {
+                setIsHasBg(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            console.log('remove event');
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <Container asChild>
-                <header>
+                <header
+                    className={cn('fixed top-0 left-0 transition-[background-color] duration-500', {
+                        'bg-background': isHasBg,
+                    })}
+                >
                     <Header />
                 </header>
             </Container>
 
-            <main>
+            <main className='min-h-[800px] bg-muted'>
                 <Outlet />
             </main>
 
