70aa8cb599b8914317a2f236d19a86369ddbfc23
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-12-14 15:06:47 +0700

    refactor(detail page): background thumbnail

--- src/view/detail/page.tsx
+++ src/view/detail/page.tsx
@@ -1,5 +1,5 @@
 import { PlayCircleIcon, PlayIcon, StarIcon, TvMinimalPlayIcon } from 'lucide-react';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { Breadcrumb, SectionEpisode } from '~components';
 import { Button } from '~components-ui/button';
@@ -20,79 +20,90 @@ function DetailPage() {
     }, [seoOnPage.titleHead]);
 
     return (
-        <div className='px-4 py-6 mt-header'>
+        <div className='p-4 mt-header'>
             <Breadcrumb breadcrumb={data.breadCrumb} />
 
             {/* Info */}
-            <div className='flex flex-col md:flex-row gap-6 mt-8'>
+            <div className='relative flex flex-col md:flex-row gap-6 mt-4 overflow-hidden'>
+                <div className='hidden sm:block absolute inset-0 blur-md opacity-30 -z-10'>
+                    <img
+                        src={'https://img.ophim.live/uploads/movies/' + data.item.poster_url}
+                        alt={data.item.name}
+                        className='size-full object-cover'
+                    />
+                </div>
+
                 <div className='relative md:w-1/4'>
                     <img
                         src={'https://img.ophim.live/uploads/movies/' + data.item.thumb_url}
                         alt={data.item.name}
-                        className='w-full h-full object-cover'
+                        className='size-full object-cover opacity-60'
                     />
 
-                    <div className='absolute left-1/2 bottom-2 -translate-x-1/2'>
-                        <Link
-                            to={`/watch/${data.item.slug}`}
-                            className='inline-flex items-center justify-center size-14 bg-primary rounded-full shadow-primary shadow-md [&_svg]:size-6'
-                        >
-                            <PlayIcon strokeWidth={1.5} fill='currentColor' className='ml-[2px]' />
-                            <span className='sr-only'>Xem phim</span>
-                        </Link>
-                    </div>
+                    {data.item.episode_current !== 'Trailer' && (
+                        <div className='absolute left-1/2 bottom-2 -translate-x-1/2'>
+                            <Link
+                                to={`/watch/${data.item.slug}`}
+                                className='inline-flex items-center justify-center size-14 bg-primary rounded-full shadow-primary shadow-md [&_svg]:size-6'
+                            >
+                                <PlayIcon strokeWidth={1.5} fill='currentColor' className='ml-[2px]' />
+                                <span className='sr-only'>Xem phim</span>
+                            </Link>
+                        </div>
+                    )}
                 </div>
-                <div className='md:w-3/4'>
+
+                <div className='md:w-3/4 pt-2'>
                     <h1 className='text-4xl font-bold text-primary'>{data.item.name}</h1>
                     <h2 className='mt-4 font-bold'>{data.item.origin_name}</h2>
 
                     <ul className='grid grid-cols-1 sm:grid-cols-2 gap-x-3 gap-y-2 mt-6'>
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Năm:</span>
-                                <span>{data.item.year}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Năm:</span>
+                            <span>{data.item.year}</span>
                         </li>
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Thời lượng:</span>
-                                <span>{data.item.time}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Thời lượng:</span>
+                            <span>{data.item.time}</span>
                         </li>
 
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Tập mới nhất:</span>
-                                <span>{data.item.episode_current}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Tập mới nhất:</span>
+                            <span>{data.item.episode_current}</span>
                         </li>
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Tổng số tập:</span>
-                                <span>{data.item.episode_total}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Tổng số tập:</span>
+                            <span>{data.item.episode_total}</span>
                         </li>
 
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Quốc gia:</span>
-                                <span>{data.item.country.map((item) => item.name).join(', ')}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Quốc gia:</span>
+                            {data.item.country.map((item, index) => (
+                                <Fragment key={item.slug}>
+                                    {index === 0 ? '' : ', '}
+                                    <Link to={`/quoc-gia/${item.slug}`} className='hover:text-primary'>
+                                        {item.name}
+                                    </Link>
+                                </Fragment>
+                            ))}
                         </li>
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Chất lượng:</span>
-                                <span>
-                                    {data.item.lang}+{data.item.quality}
-                                </span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Chất lượng:</span>
+                            <span>
+                                {data.item.lang}+{data.item.quality}
+                            </span>
                         </li>
 
                         <li>
-                            <div className='flex gap-1'>
-                                <span className='text-muted-foreground whitespace-nowrap'>Thể loại:</span>
-                                <span>{data.item.category.map((item) => item.name).join(', ')}</span>
-                            </div>
+                            <span className='mr-1 text-muted-foreground whitespace-nowrap'>Thể loại:</span>
+                            {data.item.category.map((item, index) => (
+                                <Fragment key={item.slug}>
+                                    {index === 0 ? '' : ', '}
+                                    <Link to={`/the-loai/${item.slug}`} className='hover:text-primary'>
+                                        {item.name}
+                                    </Link>
+                                </Fragment>
+                            ))}
                         </li>
                     </ul>
 
@@ -106,12 +117,14 @@ function DetailPage() {
                     </div>
 
                     <div className='flex items-center gap-3 mt-4'>
-                        <Button asChild>
-                            <Link to={`/watch/${data.item.slug}`}>
-                                <PlayCircleIcon size={16} />
-                                Xem phim
-                            </Link>
-                        </Button>
+                        {data.item.episode_current !== 'Trailer' && (
+                            <Button asChild>
+                                <Link to={`/watch/${data.item.slug}`}>
+                                    <PlayCircleIcon size={16} />
+                                    Xem phim
+                                </Link>
+                            </Button>
+                        )}
 
                         {data.item.trailer_url && (
                             <Dialog>
@@ -140,11 +153,13 @@ function DetailPage() {
             </div>
 
             {/* Episodes */}
-            <div className='mt-8 space-y-2'>
-                {episodes.map((episode) => (
-                    <SectionEpisode key={episode.server_name} episode={episode} />
-                ))}
-            </div>
+            {data.item.episode_current !== 'Trailer' && (
+                <div className='mt-8 space-y-2'>
+                    {episodes.map((episode) => (
+                        <SectionEpisode key={episode.server_name} episode={episode} />
+                    ))}
+                </div>
+            )}
 
             {/* Description */}
             <h3 className='mt-4 text-xl font-bold text-primary underline underline-offset-8'>Nội dung phim</h3>
